04437741d49a960f40d473145f47f8eb47171dd3
Leigh Johnson <hi@leighjohnson.me>
2020-07-19 13:37:24 -0700

    Revert "Refactor 3-stage DisparityNetwork layer into modular single-stage Layers (#11)" (#14)
    This reverts commit b82f3f97e8c5d21b62351db6079556211b4fb2eb.

--- tf_anynet/models/metrics.py
+++ tf_anynet/models/metrics.py
@@ -2,23 +2,15 @@
 import tensorflow as tf
 from tensorflow import keras
 
-@keras.utils.register_keras_serializable(package='AnyNet')
 class L1DisparityMaskLoss(keras.losses.Loss):
 
     __name__ = 'L1DisparityMaskLoss'
-    def __init__(self, stage, global_max_disp, *args, **kwargs):
+    def __init__(self, stage, global_max_disp):
         super(L1DisparityMaskLoss, self).__init__()
         self.stage = stage
         self.global_max_disp = global_max_disp
         self.loss = keras.losses.Huber()
 
-    def get_config(self):
-        config = {
-            'stage': self.stage,
-            'global_max_disp': self.global_max_disp
-        }
-        config.update(super().get_config())
-        return config
     def __call__(self, disp, logits, sample_weight=None):
         
         mask = disp < self.global_max_disp
--- tf_anynet/models/regularization.py
+++ tf_anynet/models/regularization.py
@@ -2,55 +2,20 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-
-@keras.utils.register_keras_serializable(package='AnyNet')
-class Conv3DRegularizer(keras.layers.Layer):
-
-    def __init__(self, nlayers, out_channels, *args, **kwargs):
-        super(Conv3DRegularizer, self).__init__()
-
-        self.nlayers = nlayers
-        self.out_channels = out_channels
+def conv3d_block(
+    inputs,
+    out_channels,
+    initializer_cls=keras.initializers.VarianceScaling, 
+    padding='same',
+    kernel_size=3,     
+    dilation_rate=1, 
+    batch_norm=True,
+    momentum=0.9,
+    epsilon=1e-5,
+    strides=1
+    ):
     
-    def get_config(self):
-        config = {
-            'nlayers': self.nlayers,
-            'out_channels': self.out_channels
-        }
-        config.update(super().get_config())
-        return config
-
-    def build(self, input_shapes):
-        inputs = layers.Input(shape=input_shapes[1:], name="input_conv3d_regularizer")
-        self.conv3d_net = self._build_conv3d_net(inputs, self.nlayers, self.out_channels)
-        self.model = keras.Model(inputs=inputs, outputs=self.conv3d_net)
-
-    def call(self, inputs):
-        return self.model(inputs)
-
-    def _build_conv3d_net(self, inputs, layers, out_channels):
-        net = self._build_conv3d_block(inputs, out_channels)
-        
-        for _ in range(layers):
-            net = self._build_conv3d_block(net, out_channels)
-
-        return self._build_conv3d_block(net, 1)
-    
-    def _build_conv3d_block(
-        self,
-        inputs,
-        out_channels,
-        initializer_cls=keras.initializers.VarianceScaling, 
-        padding='same',
-        kernel_size=3,     
-        dilation_rate=1, 
-        batch_norm=True,
-        momentum=0.9,
-        epsilon=1e-5,
-        strides=1
-        ):
-        
-        #if batch_norm is True:
+    if batch_norm:
         x = layers.BatchNormalization(
                 momentum=momentum,
                 epsilon=epsilon,
@@ -66,13 +31,48 @@ class Conv3DRegularizer(keras.layers.Layer):
             kernel_initializer=initializer_cls()
         )(x)
         return x
-        # else:
-        #     return layers.Conv2D(
-        #         out_channels,
-        #         kernel_size=kernel_size,
-        #         strides=strides,
-        #         padding=padding,
-        #         activation='relu',
-        #         dilation_rate=dilation_rate,
-        #         kernel_initializer=initializer_cls()
-        #     )(inputs)
+    else:
+        return layers.Conv2D(
+            out_channels,
+            kernel_size=kernel_size,
+            strides=strides,
+            padding=padding,
+            activation='relu',
+            dilation_rate=dilation_rate,
+            kernel_initializer=initializer_cls()
+        )(inputs)
+
+
+def conv3d_net(inputs, layers, out_channels):
+    net = conv3d_block(inputs, out_channels)
+    
+    for _ in range(layers):
+        net = conv3d_block(net, out_channels)
+
+    return conv3d_block(net, 1)
+
+@keras.utils.register_keras_serializable(package='AnyNet')
+class Conv3DRegularizer(keras.layers.Layer):
+
+    def __init__(self, nlayers, out_channels):
+        super(Conv3DRegularizer, self).__init__()
+
+        self.nlayers = nlayers
+        self.out_channels = out_channels
+    
+    def get_config(self):
+        config = {
+            'nlayers': self.nlayers,
+            'out_channels': self.out_channels
+        }
+        config.update(super().get_config())
+        return config
+
+    def build(self, input_shapes):
+
+        inputs = layers.Input(shape=input_shapes[1:], name="input_conv3d_regularizer")
+        self.conv3d_net = conv3d_net(inputs, self.nlayers, self.out_channels)
+        self.model = keras.Model(inputs, self.conv3d_net)
+
+    def call(self, inputs):
+        return self.model(inputs)
